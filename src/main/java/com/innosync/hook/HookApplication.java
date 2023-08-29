508486f1d9820d5316773dc145913d90eb795c26
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-29 17:57:34 +0900

    Initial commit

---
+++ src/main/java/com/innosync/hook/HookApplication.java
@@ -0,0 +1,13 @@
+package com.innosync.hook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HookApplication.class, args);
+	}
+
+}
