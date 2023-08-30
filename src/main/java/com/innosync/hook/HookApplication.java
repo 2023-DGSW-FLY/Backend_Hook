83da577e59742fa7f0ff2d0ca2034b87dd297f1c
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-30 20:23:20 +0900

    Feat :: 웹 크롤링한거 디비에 저장

--- src/main/java/com/innosync/hook/HookApplication.java
+++ src/main/java/com/innosync/hook/HookApplication.java
@@ -2,10 +2,13 @@ package com.innosync.hook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HookApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HookApplication.class, args);
 	}
+
 }
---
+++ src/main/java/com/innosync/hook/Service/ContestService.java
@@ -0,0 +1,61 @@
+package com.innosync.hook.Service;
+
+import com.innosync.hook.dto.ContestDto;
+import com.innosync.hook.entity.ContestEntity;
+import com.innosync.hook.repository.ContestRepository;
+import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class ContestService {
+    private final ContestRepository contestRepository;
+    private final ModelMapper modelMapper;
+
+    private static String CONTEST_URL = "https://www.onoffmix.com/event/main?s=%ED%95%B4%EC%BB%A4%ED%86%A4";
+
+    @PostConstruct
+    public void getContestInfo() throws IOException {
+
+        Document doc = Jsoup.connect(CONTEST_URL).get();
+        Elements contents = doc.select("#content > div > section.event_main_area > ul > li:nth-child(2) > article.event_area.event_main > a > div.event_thumbnail > img");
+        // li:nth-child(1) () 안에 값 바꿔서 게시물 순서 조정가능
+        Elements dateContents = doc.select("#content > div > section.event_main_area > ul > li:nth-child(2) > article.event_area.event_main > a > div.event_info_area > div.event_info > div.date");
+
+        if (!contents.isEmpty() && !dateContents.isEmpty()) {
+            Element imgElement = contents.get(0);
+            String altText = imgElement.attr("alt");
+            String srcUrl = imgElement.attr("src");
+
+            Element dateElement = dateContents.get(0);
+            String dateInfo = dateElement.text();
+
+            System.out.println("Alt Text: " + altText);
+            System.out.println("Image URL: " + srcUrl);
+            System.out.println("Date Info: " + dateInfo);
+
+            ContestDto contestDto = new ContestDto();
+            contestDto.setTitle(altText); // 제목
+            contestDto.setImgUrl(srcUrl); // 이미지
+            contestDto.setDateTime(dateInfo); // 시간
+
+            ContestEntity contestEntity = modelMapper.map(contestDto, ContestEntity.class);
+            contestRepository.save(contestEntity);
+
+        } else {
+            System.out.println("No content found.");
+        }
+    }
+}
