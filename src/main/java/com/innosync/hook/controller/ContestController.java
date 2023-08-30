480aa94dad9e49a3046fde894cafd0ff5ca4c7ba
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-30 21:21:27 +0900

    Refactor :: Autowired 부분 제거  -> RequiredArgsConstructor 로 수정

--- src/main/java/com/innosync/hook/controller/ContestController.java
+++ src/main/java/com/innosync/hook/controller/ContestController.java
@@ -1,5 +1,6 @@
 package com.innosync.hook.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +12,11 @@ import com.innosync.hook.Service.ContestService;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api")
 public class ContestController {
 
     private final ContestService contestService;
-
-    @Autowired
-    public ContestController(ContestService contestService) {
-        this.contestService = contestService;
-    }
 
     @GetMapping("/contest")
     public List<ContestDto> getAllContests() {
