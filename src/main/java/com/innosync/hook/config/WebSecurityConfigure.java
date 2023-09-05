467b0f42b4b9b2476fa9b1f55e0884c285e61818
최미래 <miraexhoi@gmail.com>
2023-09-05 23:32:29 +0900

    Feat :: Oauth 인증 성공 및 실패 관리

---
+++ src/main/java/com/innosync/hook/config/WebSecurityConfigure.java
@@ -0,0 +1,75 @@
+package com.innosync.hook.config;
+
+import com.innosync.hook.Service.CustomOAuth2UserService;
+import com.innosync.hook.jwt.JwtAuthenticationFilter;
+import com.innosync.hook.jwt.JwtTokenProvider;
+import com.innosync.hook.oauth2.OAuth2AuthenticationFailureHandler;
+import com.innosync.hook.oauth2.OAuth2AuthenticationSuccessHandler;
+import com.innosync.hook.repository.CookieAuthorizationRequestRepository;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+// 추가
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+// 추가 끝
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfigure {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CookieAuthorizationRequestRepository cookieAuthorizationRequestRepository;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //httpBasic, csrf, formLogin, rememberMe, logout, session disable
+        http
+                .cors()
+                .and()
+                .httpBasic().disable()
+                .csrf().disable()
+                .formLogin().disable()
+                .rememberMe().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        //요청에 대한 권한 설정
+        http.authorizeRequests()
+                .antMatchers("/oauth2/**").permitAll()
+                .anyRequest().authenticated();
+
+        //oauth2Login
+        http.oauth2Login()
+                .authorizationEndpoint().baseUri("/oauth2/authorize")  // 소셜 로그인 url
+                .authorizationRequestRepository(cookieAuthorizationRequestRepository)  // 인증 요청을 cookie 에 저장
+                .and()
+                .redirectionEndpoint().baseUri("/oauth2/callback/*")  // 소셜 인증 후 redirect url
+                .and()
+                //userService()는 OAuth2 인증 과정에서 Authentication 생성에 필요한 OAuth2User 를 반환하는 클래스를 지정한다.
+                .userInfoEndpoint().userService(customOAuth2UserService)  // 회원 정보 처리
+                .and()
+                .successHandler(oAuth2AuthenticationSuccessHandler)
+                .failureHandler(oAuth2AuthenticationFailureHandler);
+
+        http.logout()
+                .clearAuthentication(true)
+                .deleteCookies("JSESSIONID");
+
+        //jwt filter 설정
+        http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
