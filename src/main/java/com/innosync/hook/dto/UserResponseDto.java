f344cc98fc2345b9ef4c28d9756a381624deb8f5
최미래 <miraexhoi@gmail.com>
2023-09-05 23:37:10 +0900

    Feat :: User 관련 파일

---
+++ src/main/java/com/innosync/hook/dto/UserResponseDto.java
@@ -0,0 +1,18 @@
+package com.innosync.hook.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+public class UserResponseDto {
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    public static class TokenInfo {
+        private String grantType;
+        private String accessToken;
+        private Long accessTokenExpirationTime;
+        private String refreshToken;
+        private Long refreshTokenExpirationTime;
+    }
+}
