f2a2ba96b4e444447acc685e0a525ab1eca84dbb
최미래 <miraexhoi@gmail.com>
2023-09-05 23:38:16 +0900

    Feat :: 만료 시간

---
+++ src/main/java/com/innosync/hook/config/ExpireTime.java
@@ -0,0 +1,7 @@
+package com.innosync.hook.config;
+
+public class ExpireTime {
+
+    public static final long ACCESS_TOKEN_EXPIRE_TIME = 30 * 60 * 1000L;               //30분
+    public static final long REFRESH_TOKEN_EXPIRE_TIME = 7 * 24 * 60 * 60 * 1000L;     //7일
+}
