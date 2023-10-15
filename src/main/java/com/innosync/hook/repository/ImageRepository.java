d4aa9710d870cb1c7ec7551b2cd14d8bac53fdc4
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-10-15 21:18:03 +0900

    Del :: 안쓰는 로직 제거

--- src/main/java/com/innosync/hook/repository/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.innosync.hook.repository;
-
-import com.innosync.hook.entity.ImageEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ImageRepository extends CrudRepository<ImageEntity, Long> {
-}
-
