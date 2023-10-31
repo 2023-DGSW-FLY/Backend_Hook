8a0e59b55764c7b3f3bdc4ee0ac5259ee995bed9
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-10-31 13:10:47 +0900

    Fix :: Security 설정후 오류 수정
    스프링 버전 업글 후 이미지 리다이렉트 안되던 문제 수정
    원인 : 요청부분 토큰 없을시 403 반환
    수정 : requestMatcher 로 이미지 url 리다이렉트 부분 오픈하여 해결

--- src/main/java/com/innosync/hook/filter/HttpSecurityConfig.java
+++ src/main/java/com/innosync/hook/filter/HttpSecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -32,6 +33,7 @@ public class HttpSecurityConfig {
                 .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/api/v1/users/image/*").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sessionManagement ->
